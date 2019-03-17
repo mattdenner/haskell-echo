8424d696ff87c979f320e4fcf55b03e9b35953e4
Matthew Denner <matt.denner@gmail.com>
2019-03-17 15:43:29 +0000

    Handle the `-n` command line option

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,26 @@
 module Main where
 
 import System.Environment
+import Data.List
+
+-- Given an list of command line arguments, return the options and the rest
+separateOptionsFromArguments = 
+  partition (isPrefixOf "-")
+
+-- Given a list of options return the display function that'll be used
+optionsToDisplayFunction = 
+  whatDisplayFunction . find wantsNoNewline
+  where
+    wantsNoNewline "-n" = True
+    wantsNoNewline _    = False
+
+    whatDisplayFunction Nothing = putStrLn
+    whatDisplayFunction _       = putStr
 
 main :: IO ()
 main = do
   args <- getArgs
-  let joined = unwords args
-  putStrLn joined
+  let (options, words) = separateOptionsFromArguments args
+  let display = optionsToDisplayFunction options
+  let joined = unwords words
+  display joined
